19bdccb3b58b7f5db3596a8a40f9da6e9ee00c37
Dima Serhiienko <dima.sergienko@gmail.com>
2023-05-30 14:55:39 +0300

    Create login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { SignupDto } from './dto/signup.dto';
+import { LoginDto, SignupDto } from './dto';
 import { SignupPipe } from './pipes/signup.pipe';
 
 @Controller('auth')
@@ -8,8 +8,12 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  @UsePipes(SignupPipe)
-  signup(@Body() newUser: SignupDto) {
+  async signup(@Body(SignupPipe) newUser: SignupDto) {
     return this.authService.signup(newUser);
+  }
+
+  @Post('login')
+  async login(@Body() user: LoginDto) {
+    return this.authService.login(user);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { SignupDto } from './dto/signup.dto';
 import * as argon2 from 'argon2'
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -9,8 +10,16 @@ export class AuthService {
 
   constructor(private readonly jwtService: JwtService) {}
 
+  findUser(username: string): User | undefined {
+    return this.users.find(u => u.username === username);
+  }
+
+  createAccessToken(username: string): { accessToken: string } {
+    return { accessToken: this.jwtService.sign({ sub: username }) };
+  }
+
   async signup(newUser: SignupDto): Promise<{ accessToken: string }> {
-    if (this.users.find(u => u.username === newUser.username)) {
+    if (this.findUser(newUser.username)) {
       throw new ConflictException(`User with username ${newUser.username} already exists`);
     }
     const user = {
@@ -20,6 +29,22 @@ export class AuthService {
       lastName: newUser.lastName,
     };
     this.users.push(user);
-    return { accessToken: this.jwtService.sign({ sub: user.username }) };
+    return this.createAccessToken(user.username);
+  }
+
+  async login(user: LoginDto): Promise<{ accessToken: string }> {
+    try {
+      const existingUser = this.findUser(user.username);
+      if (!user) {
+        throw new Error();
+      }
+      const passwordMatch = await argon2.verify(existingUser.password, user.password);
+      if (!passwordMatch) {
+        throw new Error();
+      }
+      return this.createAccessToken(user.username);
+    } catch (e) {
+      throw new UnauthorizedException('Username or password may be incorrect. Please try again');
+    }
   }
 }
